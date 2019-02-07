cfe2decb56e78812c7fc9b0d37a5d06ab309f174
Mariana <marianaplazas@live.com>
2019-02-07 03:34:43 +0000

    give it up with five

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - Prints exactly with proper grammar  a string of characters
  *
  * Return: Always 0
  */
+
 int main(void)
 {
        printf("with proper grammar, but the outcome is a piece of art,\n");
