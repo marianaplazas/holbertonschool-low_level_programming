8dcb23e52322856af1427209fb83137492b3aa7f
Mariana <marianaplazas@live.com>
2019-02-07 03:50:30 +0000

    six

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-
 /**
  * main - Prints exactly with proper grammar  a string of characters
  *
  * Return: Always 0
  */
-
 int main(void)
 {
-       printf("with proper grammar, but the outcome is a piece of art,\n");
+       printf("with proper grammar, but the outcome is a piece of art,");
        return(0);
 
 }
