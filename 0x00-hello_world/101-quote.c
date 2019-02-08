30e439fa92f2ce602376cec6a2cce774ca7c5b9f
Mariana <marianaplazas@live.com>
2019-02-08 03:50:33 +0000

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-int main(void)
-{
-	gets('exactly and that piece of art is useful" - Dora Korpar, 2015-10-19"\n"');
+
+/**
+ *main start
+ *
+ *return 0 if is sucessful
+ */
+
+int main () {
+	
+	char dor[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fwrite(dor, sizeof(char), sizeof(dor) - 1, stderr);
+  
 	return(1);
 }
