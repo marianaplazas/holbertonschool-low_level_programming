8f9a102fca205c1d52655ed579a8ab459482712b
Mariana <marianaplazas@live.com>
2019-02-08 02:45:23 +0000

    frist 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main stars the program
+ *
+ *return 0 if is success
+ */
+
+int main(void)
+{
+	char az='a';
+
+	while (az <= 'z')
+	{
+		putchar(az);
+		az++;
+	}
+	putchar('\n');
+	return(0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,11 @@
+#include <stidio.h>
+
+/**
+ *main strat
+ *
+ *return always 0 if is sucess
+ */
+int main(void)
+{
+
+}
