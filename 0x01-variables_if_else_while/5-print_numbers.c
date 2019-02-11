f501ae2f1518f10a3bb89930c986d7a9115ddd5c
Mariana <marianaplazas@live.com>
2019-02-11 00:11:48 +0000

    ponits correct

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+B#include <stdio.h>
+/**
+ *main - stars
+ *
+ *return =(0) if is success
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 48 ; a < 58 ; a++);
+	{
+	printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - stars
+ *
+ *return =(0) if is success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+
+	{
+		putchar (a);
+	}
+	putchar ('\n');
+	return (0);
+}
