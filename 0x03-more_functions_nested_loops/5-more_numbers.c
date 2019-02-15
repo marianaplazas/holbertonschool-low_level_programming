523babcc01e306d77330c8d44608852b7c91f54d
Mariana <marianaplazas@live.com>
2019-02-15 04:07:56 +0000

    why if i dont put the keys works?

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -7,10 +7,19 @@
 void more_numbers(void)
 {
 	int a;
+	int ten;
 
-	for(a = '0' ; a < '9' ; a++)
+	ten = 0;
+
+	while (ten < 10)
 	{
-		_putchar(a);
+		for (a = 0; a < 15; a++)
+		{
+			if (a >= 10)
+				_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+		}
+		_putchar(10);
+		ten++;
 	}
-	_putchar(10);
 }
