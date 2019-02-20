0b64680a5f193d29b17c094092de2447299963ed
Mariana <marianaplazas@live.com>
2019-02-20 23:08:41 +0000

    five

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -6,5 +6,19 @@
  */
 void rev_string(char *s)
 {
+	int save;
+	int decrease = 0;
+	int increase = 0;
 
+	while (s[decrease])
+		decrease++;
+	decrease--;
+	while (increase != decrease && increase < decrease)
+	{
+		save = s[decrease];
+		s[decrease] = s[increase];
+		s[increase] = save;
+		decrease--;
+		increase++;
+	}
 }
