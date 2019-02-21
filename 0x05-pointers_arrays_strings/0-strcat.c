238b473cb7cb9dd42401b9aedfb4934f5cff5b75
Mariana <marianaplazas@live.com>
2019-02-21 22:05:56 +0000

    one.2

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,25 @@
 #include "holberton.h"
 /**
- *_strcat - concatenates two strings.
+ *_strcat - concatenates two strings
+ *
+ *@dest: frist pointer
+ *@src: second pointer
+ *
+ *Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
 	int one;
 	int two;
 
-	for(one = 0; dest[one] != '\0'; one++)
+	for (one = 0; dest[one] != '\0'; one++)
 	{
 
 	}
-	for(two = 0; src[two] != '\0'; two++)
+	for (two = 0; src[two] != '\0'; two++)
 	{
-		dest[one + two] = src[two];	
+		dest[one + two] = src[two];
 	}
-	dest[one +two] = '\0';
-	return(dest);
+	dest[one + two] = '\0';
+	return (dest);
 }
