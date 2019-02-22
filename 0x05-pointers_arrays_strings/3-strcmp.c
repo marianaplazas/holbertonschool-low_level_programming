89dd971f052d45a61f746d4987e8e1fe0a38e5f2
Mariana <marianaplazas@live.com>
2019-02-22 05:38:02 +0000

    betty says too much characters bye counter hi c

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -4,14 +4,14 @@
  *
  *@s1: frist pointer
  *@s2: second pointer
- *
+ *the c was counter but Betty says a lot of characters
  *Return: dest
  */
 int _strcmp(char *s1, char *s2)
 {
-	int count;
+	int c;
 
-	for (count = 0; s1[count] != '\0' && s2[count] != '\0' && s1[count] == s2[count]; count++)
+	for (c = 0; s1[c] != '\0' && s2[c] != '\0' && s1[c] == s2[c]; c++)
 	{
 	}
 	return (s1[count] - s2[count]);
