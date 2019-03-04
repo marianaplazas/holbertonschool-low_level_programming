8d21be4e40371e4acfe15c55eb4eba00cc4dc719
Mariana <marianaplazas@live.com>
2019-03-04 04:36:50 +0000

    two

--- 0x09-argc_argv/2-args.c
+++ 0x09-argc_argv/2-args.c
@@ -11,11 +11,7 @@ int main(int argc, char *argv[])
 {
 	int a;
 
-	if (argc > 0)
-	{
-		for (a = 0; a < argc; a++)
-		{
-			printf("%s\n", argv[a]);
-		}
-	}
+	for (a = 0; a < argc; a++)
+	printf("%s\n", argv[a]);
+	return (0);
 }
