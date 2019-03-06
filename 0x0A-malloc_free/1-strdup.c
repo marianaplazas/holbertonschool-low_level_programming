c7bc1add56c90124d850f00ee659adb7368f6815
Mariana <marianaplazas@live.com>
2019-03-06 03:29:18 +0000

    second

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -12,6 +12,14 @@ char *_strdup(char *str)
 	char *s;
 	int a, b;
 
-	for (
-
+	for (a = 0; str[a] != '\0'; a++)
+	{
+	}
+	a++;
+	s = malloc(sizeof(char) * a);
+	if (s == NULL)
+		return (NULL);
+	for (b = 0; b < a; b++)
+		s[b] = str[b];
+	return (s);
 }
