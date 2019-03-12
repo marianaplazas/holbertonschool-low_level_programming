dbe1959847a69047c597704a8749a614548d5e3f
Mariana <marianaplazas@live.com>
2019-03-12 03:30:16 +0000

    second with ? :

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -2,16 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * struct my_struct - Short description
- * @a: First member
- * @b: Second member
- *
- * Description: Longer description
+ * print_dog - print the dog's data hello dog :)
+ * @d: pointer to the struct
  */
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-
+		printf("Name: ");
+		(d->name == NULL) ? printf("(nil)\n") : printf("%s\n", d->name);
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: ");
+		(d->owner == NULL) ? printf("%p\n", d->owner) : printf("%s\n", d->owner);
 	}
 }
