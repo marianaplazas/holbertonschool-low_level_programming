b1f977dcb9e0b5e0dcf265635f9eabea5a6a9217
Mariana <marianaplazas@live.com>
2019-03-16 21:14:22 +0000

    frist two

---
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+/**
+ *sum_them_all - sum all the arguments
+ *
+ *@n: the arguments
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	int sum = 0;
+	unsigned int a;
+
+	va_start(list, n);
+
+	for (a = 0; a < n; a++)
+	{
+		sum += va_arg(valist, int);
+	}
+	va_end(list);
+	return (sum);
+}
---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ *print_numbers - print all numbers
+ *
+ *@separator: pointer
+ *@n: number
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int a;
+	va_list list;
+
+	va_start(list, n);
+	for (a = 0; a < n; a++)
+	{
+		printf("%d", va_arg(list, int));
+		if (a != (a - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(list)
+}
