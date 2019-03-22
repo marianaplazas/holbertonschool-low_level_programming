d909a2d9be21d9db850ac316bbc01d9678a80037
Mariana <marianaplazas@live.com>
2019-03-22 03:47:47 +0000

    fixes

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -5,16 +5,16 @@
  *
  *@head: pointer to the head of the list
  */
-void free_list(list_t *head)
+size_t free_list(list_t *head)
 {
-	list_t *aux;
+	size_t new_n;
 
-	aux = head;
-	while (aux != NULL)
+	new_n = 0;
+	while (h != NULL)
 	{
-		head = aux->next;
-		free(aux->str);
-		free(aux);
-		aux = head;
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		new_n++;
 	}
+	return (new_n);
 }
