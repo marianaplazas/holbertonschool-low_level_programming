0f949cbc425cc40ab42c47ee9dc76853374f9153
Mariana <marianaplazas@live.com>
2019-03-27 04:15:22 +0000

    add half

---
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * delete_nodeint_at_index - delete index
+ *
+ * @head: pointer to the head
+ * @index: delete
+ *
+ * Return: 1 if success or -1 
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
