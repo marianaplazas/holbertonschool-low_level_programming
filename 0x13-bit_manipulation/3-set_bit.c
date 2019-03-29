dbb250ae26ce58e3b5299371e90ea192304327dc
Mariana <marianaplazas@live.com>
2019-03-29 03:44:15 +0000

    third and fix the header

---
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * set_bit - sets acording with the index
+ *
+ *@n: the number
+ *@index: uis the index
+
+ *Return: 1 if it worked, or -1 if an error occurred
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == 0 || !n)
+		return (-1);
+	else if (index > (sizeof(*n) * 8) - 1)
+		return (-1);
+	else
+		*n = *n | 1 << index;
+	return (1);
+}
