cc01a2a0ecf609d86a6baf43cc2ee8ee982edeae
Mariana <marianaplazas@live.com>
2019-03-29 04:08:05 +0000

    fix a letter

--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -1,5 +1,5 @@
 #ifndef HOLBERTON_H
-#define HOLBERTON_
+#define HOLBERTON_H
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
