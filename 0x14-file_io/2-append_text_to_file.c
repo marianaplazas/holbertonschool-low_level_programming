cd0a1f4e726645fed05d34065616d262fa57b2ec
Mariana <marianaplazas@live.com>
2019-04-03 04:13:03 +0000

    fix 2

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@
  *
  * @filename: pointer to the file that recive
  * @text_content: the things that write
-
+ *
  * Return: 1 if is success or -1
  */
 int append_text_to_file(const char *filename, char *text_content)
@@ -29,7 +29,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		return (1);
-	for (counter = 0; text_content[counter]; counter++);
+	for (counter = 0; text_content[counter]; counter++)
 	{
 	}
 	writef = write(readf, text_content, counter);
