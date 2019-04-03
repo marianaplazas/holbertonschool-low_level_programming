6a0b395d161466e70696eb34d88ec82d0c80cc68
Mariana <marianaplazas@live.com>
2019-04-03 04:58:37 +0000

    fix betty

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
  * read_textfile - reads a text file
- * 
+ *
  * @filename: pointer to the text that recive
  * @letters: structure of size
-
+ *
  * Return: the file to read
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file = 0; 
+	int file = 0;
 	int readf = 0;
 	int writef = 0;
 	int closef = 0;
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (file == -1)
 		return (0);
 	readf = read(file, buff, letters);
-	if (readf== -1)
+	if (readf == -1)
 		return (0);
 	writef = write(STDOUT_FILENO, buff, readf);
 	if (writef == -1)
