b31c6f0ad2d8620a3ea2442116efd3c83d006df6
Mariana <marianaplazas@live.com>
2019-07-24 20:53:09 +0000

    add point 15

--- 0x1D-binary_trees/15-binary_tree_is_full.c
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -1,11 +1,19 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_is_full -
- *
- * Return: 
+ *binary_tree_is_full - measures the is_tree factor of a binary tree
+ *@tree: pointer to the tree
+ *Return: is_full factor
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-
+	int height_l;
+	int height_r;
+	int compare;
+	height_l = tree->left ? 1 + binary_tree_is_full(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_is_full(tree->right) : 0;
+	compare = height_l - height_r;
+	if (compare == 0)
+		return (1);
+	else
+		return (0);
 }
