898811c387973231bbd6d99c2612bf9a3700f947
Mariana <marianaplazas@live.com>
2019-07-24 21:20:23 +0000

    add point 18

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -1,11 +1,13 @@
 #include "binary_trees.h"
-
+#include "17-binary_tree_sibling.c"
 /**
- * binary_tree_uncle -
- *
- * Return: 
+ * binary_tree_uncle - find the uncle of a node
+ *@node: pointer to the node
+ * Return: the uncle
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	return (binary_tree_sibling(node->parent));
 }
