50e5f5cb284fa84e145230a07863684498c90326
Mariana <marianaplazas@live.com>
2019-07-24 22:17:31 +0000

    fix betty and add description 4

--- 0x1D-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1D-binary_trees/4-binary_tree_is_leaf.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf -
- *
- * Return: 
+ *binary_tree_is_leaf - checks if a node is a leaf
+ *@node: pointer to the tree
+ *Return: 1 if is a leaf or 0
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
