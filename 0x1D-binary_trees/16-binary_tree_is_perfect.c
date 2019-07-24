d57a5624aed17d8c14c0c77192383ae9228f995e
Mariana <marianaplazas@live.com>
2019-07-24 23:00:07 +0000

    add the best point 16

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -1,11 +1,13 @@
 #include "binary_trees.h"
-
+#include "15-binary_tree_is_full.c"
+#include "9-binary_tree_height.c"
 /**
- * binary_tree_is_perfect -
- *
- * Return: 
+ *binary_tree_is_perfect - checks if tree is perfect
+ *@tree: pointer to the tree
+ *Return: the height
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-
+	return (tree == NULL ? 0 : binary_tree_is_full(tree) &&
+		(binary_tree_height(tree->left) == binary_tree_height(tree->right)));
 }
