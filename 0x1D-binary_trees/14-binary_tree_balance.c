edf68b12669e79784a6b2fedf17404dcd0f9441b
Mariana <marianaplazas@live.com>
2019-07-24 22:55:48 +0000

    fixes conditions in 14

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -10,6 +10,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	size_t height_l;
 	size_t height_r;
 
+	if (tree == NULL || tree->left == NULL || tree->right == NULL)
+		return (0);
 	height_l = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
 	return (height_l - height_r);
