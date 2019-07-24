a2ed23bda92da7d2ae75a2c024673b38cfda0e0e
Mariana <marianaplazas@live.com>
2019-07-24 22:35:38 +0000

    fix condition and betty

--- 0x1D-binary_trees/10-binary_tree_depth.c
+++ 0x1D-binary_trees/10-binary_tree_depth.c
@@ -3,12 +3,14 @@
 /**
  *binary_tree_depth - measures the depth of a binary tree
  *@tree: pointer to the tree
- *Return: the depth 
+ *Return: the depth
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth_p;
 
+	if (tree == NULL)
+		return (0);
 	depth_p = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
 	return (depth_p);
 }
