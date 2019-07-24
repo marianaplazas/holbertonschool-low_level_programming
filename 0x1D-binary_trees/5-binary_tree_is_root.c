9bd5bd37324e9463cc6fc4d4d3b579489cff30d9
Mariana <marianaplazas@live.com>
2019-07-24 22:21:11 +0000

    fix betty 5

--- 0x1D-binary_trees/5-binary_tree_is_root.c
+++ 0x1D-binary_trees/5-binary_tree_is_root.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_root -
- *
- * Return: 
+ *binary_tree_is_root - checks if a given node is a root
+ *@node: pointer to the node
+ *Return: 1 if is a root or 0
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
-		return(0);
-	if(node->parent == NULL)
-		return(1);
-	return(0);
+		return (0);
+	if (node->parent == NULL)
+		return (1);
+	return (0);
 }
