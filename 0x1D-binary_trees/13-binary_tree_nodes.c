0767e80896807cc2d3543f7159becb117697a972
Mariana <marianaplazas@live.com>
2019-07-24 22:44:35 +0000

    fix betty 13

--- 0x1D-binary_trees/13-binary_tree_nodes.c
+++ 0x1D-binary_trees/13-binary_tree_nodes.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_nodes - counts nodes with at least one child
- *
+ *@tree: the pointer to the tree
  * Return: the number of nodes
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
@@ -11,7 +11,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if(tree->right != NULL || tree->left != NULL)
-		counter += binary_tree_nodes(tree->left) + 1 + binary_tree_nodes(tree->right);
-	return(counter);
+	if (tree->right != NULL || tree->left != NULL)
+		counter += binary_tree_nodes(tree->left) + 1
+			+ binary_tree_nodes(tree->right);
+	return (counter);
 }
