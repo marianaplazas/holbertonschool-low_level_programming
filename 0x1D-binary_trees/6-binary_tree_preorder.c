892059f4fcfb9d5b7cba28fca2675cb86206a613
Mariana <marianaplazas@live.com>
2019-07-24 22:25:05 +0000

    fix betty 6

--- 0x1D-binary_trees/6-binary_tree_preorder.c
+++ 0x1D-binary_trees/6-binary_tree_preorder.c
@@ -1,9 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder -
- *
- * Return: 
+ * binary_tree_preorder - goes through a binary tree using pre-order traversal
+ *@func: the function
+ *@tree: pointer to the tree
+ * Return: nothing
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
