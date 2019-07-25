f591df7320cb3abe6bd2098e831460dbc743ae9b
Mariana <marianaplazas@live.com>
2019-07-25 17:03:06 +0000

    fix the 16 point

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -8,6 +8,11 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	return (tree == NULL ? 0 : binary_tree_is_full(tree) &&
-		(binary_tree_height(tree->left) == binary_tree_height(tree->right)));
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL || tree->right == NULL)
+		return (1);
+	else
+		return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right) &&
+			(binary_tree_height(tree->left) == binary_tree_height(tree->right)));
 }
