a91cf6c84a11af3b25fc4b70c03b902bc005145b
Mariana <marianaplazas@live.com>
2019-07-26 19:35:17 +0000

    add 110 and 113

--- 0x1D-binary_trees/113-bst_search.c
+++ 0x1D-binary_trees/113-bst_search.c
@@ -1,11 +1,18 @@
 #include "binary_trees.h"
-
 /**
- * bst_search -
- *
- * Return: 
+ * bst_search - Search for a value in a Binary Search Tree
+ * @tree: pointer to the tree
+ * @value: the value to look
+ * Return: the value
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
-
+	if (tree == NULL)
+		return (NULL);
+	if (tree->n == value)
+		return ((bst_t *)tree);
+	if (value < tree->n)
+		return (bst_search(tree->left, value));
+	else
+		return (bst_search(tree->right, value));
 }
