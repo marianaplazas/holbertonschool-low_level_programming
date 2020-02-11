81dd59da95a83aa672afb40ea5faa5a2998c7808
Mariana Plazas <47125098+marianaplazas@users.noreply.github.com>
2020-02-10 22:27:12 -0500

    change file to the correct

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,42 +1,41 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * delete_nodeint_at_index - deletes the node according to the index
- *
- * @head: pointer to head
- * @index: element to delete
- * Return: 1 if success or -1 if fail
+  * insert_nodeint_at_index - inserts a new node at a given position
+  * @head: pointer to a pointer
+  * @idx: takes in the index
+  * @n: takes in an integer
+  * Return: NULL if function fail
  */
-
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *aux;
-	listint_t *delete;
-	unsigned int a = 0;
+	listint_t *newNode;
+	listint_t *temp;
+	unsigned int counter = 0;
 
-	aux = *head;
-	if (*head == NULL)
-		return (-1);
-	if (index == 0)
+	if (head == NULL)
+		return (NULL);
+	newNode = malloc(sizeof(listint_t));
+	newNode->n = n;
+	if (newNode == NULL)
+		return (NULL);
+	if (idx == 0)
 	{
-		*head = (*head)->next;
-		free(aux);
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
 	}
-	else
+	temp = *head;
+	while (temp != NULL)
 	{
-		while (a < index - 1)
+		if (counter + 1 == idx)
 		{
-			aux = aux->next;
-			if (aux == NULL)
-				return (-1);
-			a++;
+			newNode->next = temp->next;
+			temp->next = newNode;
+			return (newNode);
 		}
-		delete = aux;
-		delete = delete->next;
-		aux->next = delete->next;
-		free(delete);
+		temp = temp->next;
+		counter++;
 	}
-	return (1);
+	free(newNode);
+	return (NULL);
 }
